1fbfae5801b00e70b3055b7d7bb5e28032398fd5
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-05-30 13:36:22 +0200

    implement different routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainContentComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy', component: PrivacyComponent }
+];
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
