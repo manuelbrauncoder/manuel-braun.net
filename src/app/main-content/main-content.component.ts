46dedc8777e4f05b871277945c1356b5f1a22d4b
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-05-30 21:36:10 +0200

    implement about me section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AtfComponent } from '../atf/atf.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
+import { SkillsComponent } from '../skills/skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [AtfComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
