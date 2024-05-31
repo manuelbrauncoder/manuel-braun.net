e4e7d8decb582992f7ccd241dd3a59a9fbd421ce
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-05-31 22:12:36 +0200

    test angular projection in portfolio component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { AtfComponent } from '../atf/atf.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsComponent } from '../skills/skills.component';
+import { ContactComponent } from '../contact/contact.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AtfComponent, AboutMeComponent, SkillsComponent],
+  imports: [AtfComponent, AboutMeComponent, SkillsComponent, ContactComponent, PortfolioComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
