15ade3ab427e50e33541420709fe682880fa0517
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-06-03 22:00:13 +0200

    start with responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import {
+  Breakpoints,
+  BreakpointObserver,
+  BreakpointState,
+} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  isHandsetPortrait: boolean = false;
+
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.responsive.observe(Breakpoints.HandsetPortrait).subscribe((result) => {
+      if (result.matches) {
+        console.log('screens matches HandsetLandscape');
+        this.isHandsetPortrait = true;
+      } else if (!result.matches) {
+        this.isHandsetPortrait = false;
+      }
+    });
+  }
 }
