5ae06983a024677a15da133ad24afa2aa25bfea9
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-06-09 21:37:47 -0700

    implement scroll service to header and mobile header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { ScrollToAnchorService } from '../services/scroll-to-anchor.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private ScrollToAnchorService: ScrollToAnchorService) {}
 
+  scrollToElement(element: string) {
+    this.ScrollToAnchorService.scrollToAnchor(element, -100);
+  }
 }
