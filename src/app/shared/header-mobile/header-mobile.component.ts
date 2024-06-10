9148854c13ec2b74f7355a352b772892fa56a577
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-06-09 18:55:11 -0700

    build burger menu

--- src/app/shared/header-mobile/header-mobile.component.ts
+++ src/app/shared/header-mobile/header-mobile.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header-mobile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-mobile.component.html',
-  styleUrl: './header-mobile.component.scss'
+  styleUrl: './header-mobile.component.scss',
 })
 export class HeaderMobileComponent {
+  showSidebar: boolean = false;
 
+  toggleSidebar(){
+    if (this.showSidebar) {
+      this.showSidebar = false;
+    } else {
+      this.showSidebar = true;
+    }
+  }
 }
