3114bc87e9a182f350a4c8b3dc4d886e5c1c0e2b
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-06-14 19:22:41 -0600

    implement intersectionObserver with slide in animation for skills section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,14 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { SkillsService } from '../shared/services/skills.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
-export class SkillsComponent {
+export class SkillsComponent implements AfterViewInit {
 
-  skills = inject(SkillsService)
+  elementVisible: boolean = false;
+
+  @ViewChild('slideInDiv') slideInElement!: ElementRef;
+
+  skills = inject(SkillsService);
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    let observerOptions = {
+      root: null, // Standard: Ansichtport
+      rootMargin: '0px 0px -300px 0px', // 200px Offset vom unteren Rand
+      threshold: 0 // 0 bedeutet, dass sobald irgendein Teil des Zielobjekts sichtbar ist, der Callback aufgerufen wird
+    };
+
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.elementVisible = true;
+        }
+      });
+    }, observerOptions);
+
+    observer.observe(this.slideInElement.nativeElement);
+  }
 }
