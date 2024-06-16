fc49b304cac77af8e556a6a11dee72bc2ef69e00
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-06-16 08:08:37 -0600

    atf section still buggy in mobile

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  inject,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { SkillsService } from '../shared/services/skills.service';
 import { CommonModule } from '@angular/common';
 
@@ -7,32 +13,37 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent implements AfterViewInit {
-
   elementVisible: boolean = false;
 
   @ViewChild('slideInDiv') slideInElement!: ElementRef;
 
   skills = inject(SkillsService);
 
+  observerOptions = {
+    root: null, // Standard: Ansichtport
+    rootMargin: '0px 0px -300px 0px', // 200px Offset vom unteren Rand
+    threshold: 0, // 0 bedeutet, dass sobald irgendein Teil des Zielobjekts sichtbar ist, der Callback aufgerufen wird
+  };
+
   constructor() {}
 
   ngAfterViewInit() {
-    let observerOptions = {
-      root: null, // Standard: Ansichtport
-      rootMargin: '0px 0px -300px 0px', // 200px Offset vom unteren Rand
-      threshold: 0 // 0 bedeutet, dass sobald irgendein Teil des Zielobjekts sichtbar ist, der Callback aufgerufen wird
-    };
+    this.startObserver();
+  }
 
+  startObserver() {
     let observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           this.elementVisible = true;
+          console.log('show');
+          
         }
       });
-    }, observerOptions);
+    }, this.observerOptions);
 
     observer.observe(this.slideInElement.nativeElement);
   }
