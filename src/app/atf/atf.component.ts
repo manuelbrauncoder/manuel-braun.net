293ac2d51b47bbd8b8192116d4f9d5b1d7037d08
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-05 23:08:51 +0200

    implement hostlistener for shape positioning

--- src/app/atf/atf.component.ts
+++ src/app/atf/atf.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Renderer2, OnDestroy, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { TsNgxService } from '../shared/services/ts-ngx.service';
@@ -11,17 +11,42 @@ import { ScrollToAnchorService } from "../shared/services/scroll-to-anchor.servi
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss',
 })
-export class AtfComponent {
+export class AtfComponent implements AfterViewInit, OnDestroy {
   currentLanguage: string;
+  @ViewChild('myImg') myImg!: ElementRef;
+  @ViewChild('shapeImg') shapeImg!: ElementRef;
+  offset: number = 500;
 
-  constructor(public translationService: TsNgxService, private ScrollToAnchorService: ScrollToAnchorService) {
+  constructor(public translationService: TsNgxService,
+    private scrollToAnchorService: ScrollToAnchorService,
+    private renderer: Renderer2
+  ) {
     this.currentLanguage = this.translationService.currentLang;
     this.translationService.onLangChange.subscribe((event) => {
       this.currentLanguage = event.lang;
     });
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.calculatePosition();    
+  }
+
   scrollToElement(element: string) {
-    this.ScrollToAnchorService.scrollToAnchor(element, -100);
+    this.scrollToAnchorService.scrollToAnchor(element, -100);
+  }
+
+  ngAfterViewInit(): void {
+    this.calculatePosition();
+  }
+
+  calculatePosition(){
+    const referenceRect = this.myImg.nativeElement.getBoundingClientRect();
+    const bottomOfReference = referenceRect.bottom - this.offset;
+    this.renderer.setStyle(this.shapeImg.nativeElement, 'top', `${bottomOfReference}px`);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.onResize.bind(this));
   }
 }
