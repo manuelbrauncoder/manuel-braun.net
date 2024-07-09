5badbec4b218c3461f627cebd1f90e08678812cf
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-09 16:49:52 +0200

    style mobile landscape component

--- src/app/nolandscapemode/nolandscapemode.component.ts
+++ src/app/nolandscapemode/nolandscapemode.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TsNgxService } from '../shared/services/ts-ngx.service';
 
 @Component({
   selector: 'app-nolandscapemode',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './nolandscapemode.component.html',
-  styleUrl: './nolandscapemode.component.scss'
+  styleUrl: './nolandscapemode.component.scss',
 })
 export class NolandscapemodeComponent {
+  currentLanguage: string;
 
+  constructor(public translationService: TsNgxService) {
+    this.currentLanguage = this.translationService.currentLang;
+    this.translationService.onLangChange.subscribe((event) => {
+      this.currentLanguage = event.lang;
+    });
+  }
 }
