fa9d4b6992d487b4cbd2e3bac613d1b0cbcd6a5e
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-09 17:33:36 +0200

    delete empty space and comments

--- src/app/atf/atf.component.ts
+++ src/app/atf/atf.component.ts
@@ -1,12 +1,4 @@
-import {
-  Component,
-  AfterViewInit,
-  ViewChild,
-  ElementRef,
-  Renderer2,
-  OnDestroy,
-  HostListener,
-} from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { TsNgxService } from '../shared/services/ts-ngx.service';
@@ -19,11 +11,8 @@ import { ScrollToAnchorService } from '../shared/services/scroll-to-anchor.servi
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss',
 })
-export class AtfComponent implements AfterViewInit, OnDestroy {
+export class AtfComponent {
   currentLanguage: string;
-  @ViewChild('myImg') myImg!: ElementRef;
-  @ViewChild('shapeImg') shapeImg!: ElementRef;
-  offset: number = 500;
 
   constructor(
     public translationService: TsNgxService,
@@ -37,41 +26,11 @@ export class AtfComponent implements AfterViewInit, OnDestroy {
   }
 
   /**
-   * call method on resizing
-   * @param event 
-   */
-  // @HostListener('window:resize', ['$event'])
-  // onResize(event: Event) {
-  //   this.calculatePosition();
-  // }
-
-  /**
    * scroll to element with offset
    * @param element 
    */
   scrollToElement(element: string) {
     this.scrollToAnchorService.scrollToAnchor(element, -100);
-  }
-
-  ngAfterViewInit(): void {
-    //this.calculatePosition();
-  }
-
-  /**
-   * calculate position from shape image
-   */
-  calculatePosition() {
-    const referenceRect = this.myImg.nativeElement.getBoundingClientRect();
-    const bottomOfReference = referenceRect.bottom - this.offset;
-    this.renderer.setStyle(
-      this.shapeImg.nativeElement,
-      'top',
-      `${bottomOfReference}px`
-    );
-  }
-
-  ngOnDestroy(): void {
-    //window.removeEventListener('resize', this.onResize.bind(this));
   }
 }
 
