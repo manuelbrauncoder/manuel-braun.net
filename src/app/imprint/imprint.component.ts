9b9e82c0642c81b38fa549fea8f1ef31aa9d57a0
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-10 18:23:53 +0200

    translate imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { ScrollToAnchorService } from '../shared/services/scroll-to-anchor.service';
+import { TsNgxService } from '../shared/services/ts-ngx.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+  currentLanguage: string;
 
-  constructor(public scrollToAnchorService: ScrollToAnchorService){}
+  constructor( public translationService: TsNgxService, public scrollToAnchorService: ScrollToAnchorService){
+    this.currentLanguage = this.translationService.currentLang;
+    this.translationService.onLangChange.subscribe((event) => {
+      this.currentLanguage = event.lang;
+    });
+  }
 }
