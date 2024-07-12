2e31237a4c83630e85f356930e76714a01d6036d
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-12 22:01:32 +0200

    translate privace and imprint, improve atf gradient svg

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { ScrollToAnchorService } from '../shared/services/scroll-to-anchor.service';
+import { TsNgxService } from '../shared/services/ts-ngx.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
 export class PrivacyComponent {
+  currentLanguage: string;
 
-  constructor(public scrollToAnchorService: ScrollToAnchorService){}
+  constructor( public translationService: TsNgxService, public scrollToAnchorService: ScrollToAnchorService){
+    this.currentLanguage = this.translationService.currentLang;
+    this.translationService.onLangChange.subscribe((event) => {
+      this.currentLanguage = event.lang;
+    });
+  }
 }
